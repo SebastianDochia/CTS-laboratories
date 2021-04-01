cc1f58c43a99c1d72d59ff11716c83d148c77494
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-01 15:20:24 +0300

    Lab 06 Part 02

--- src/ro/ase/csie/cts/labs/lab06/builder/SuperHero.java
+++ src/ro/ase/csie/cts/labs/lab06/builder/SuperHero.java
@@ -17,7 +17,7 @@ public class SuperHero {
 
     }
 
-    public SuperHero(String name, int lifePoints, boolean isVillain, boolean isWounded, WeaponInterface rightWeapon, WeaponInterface leftWeapon, SuperPowerInterface superPower, SuperPowerInterface superSuperPower) {
+    private SuperHero(String name, int lifePoints, boolean isVillain, boolean isWounded, WeaponInterface rightWeapon, WeaponInterface leftWeapon, SuperPowerInterface superPower, SuperPowerInterface superSuperPower) {
         this.name = name;
         this.lifePoints = lifePoints;
         this.isVillain = isVillain;
@@ -26,5 +26,50 @@ public class SuperHero {
         this.leftWeapon = leftWeapon;
         this.superPower = superPower;
         this.superSuperPower = superSuperPower;
+    }
+
+    public static class SuperHeroBuilder {
+        SuperHero superHero;
+
+        public SuperHeroBuilder(String name, int lifePoints) {
+            this.superHero = new SuperHero();
+            this.superHero.name = name;
+            this.superHero.lifePoints = lifePoints;
+        }
+
+        public SuperHeroBuilder isVillain() {
+            this.superHero.isVillain = true;
+            return this;
+        }
+
+        public SuperHeroBuilder isWounded() {
+            this.superHero.isWounded = true;
+            return this;
+        }
+
+        public SuperHeroBuilder setRightWeapon(WeaponInterface weapon) {
+            this.superHero.rightWeapon = weapon;
+            return this;
+        }
+
+        public SuperHeroBuilder setLeftWeapon(WeaponInterface weapon) {
+            this.superHero.leftWeapon = weapon;
+            return this;
+        }
+
+        public SuperHeroBuilder setSuperPower(SuperPowerInterface power) {
+            this.superHero.superPower = power;
+            return this;
+        }
+
+        public SuperHeroBuilder setSuperSuperPower(SuperPowerInterface power) {
+            this.superHero.superSuperPower = power;
+            return this;
+        }
+
+        public SuperHero build() {
+            //return new SuperHero(*pass all variables*);
+            return this.superHero;
+        }
     }
 }
--- src/ro/ase/csie/cts/labs/lab06/builder/TestBuilder.java
+++ src/ro/ase/csie/cts/labs/lab06/builder/TestBuilder.java
@@ -7,5 +7,9 @@ public class TestBuilder {
     // init it
     //TO DO: don't forget
 
-    SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), null, new Flying(), null);
+    //SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), null, new Flying(), null);
+
+    SuperHero superman = new SuperHero.SuperHeroBuilder("Superman", 100).build();
+
+    SuperHero joker = new SuperHero.SuperHeroBuilder("Joker", 200).isVillain().isWounded().setRightWeapon(new Weapon()).build();
 }
